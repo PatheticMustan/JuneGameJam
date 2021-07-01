81b2e15e5bef808abb479be4ea65fda158d5d37b
Mustan Pathetic <patheticmustan@gmail.com>
2021-07-01 16:50:46 -0400

    broken beat code

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -18,13 +18,41 @@ public class GameManagerScript : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.Space)) click();
 
         if (Input.GetKeyDown(KeyCode.K)) startLevel(0);
+
+        Debug.Log(1 - (bms.time % (1 / bms.beatsPerSecond)));
     }
 
     void click() {
         GetComponent<AudioSource>().Play();
 
-        Debug.Log(bms.time % (1/bms.beatsPerSecond));
-        //bms.currentBeat;
+        if (bms.playing) {
+            // how good a hit is (lower is better)
+            // ranges from -1 to 1
+            float clickTiming = 1 - (bms.time % (1 / bms.beatsPerSecond));
+            // ranges from 0 to 1
+            float cta = Mathf.Abs(clickTiming);
+
+            Debug.Log(clickTiming);
+
+            /** Beat Timings
+             * 1 to 0.5:        too early
+             * 0.5 to 0.25:     good
+             * 0.25 to -0.25:   perfect
+             * -0.25 to -0.5:   good
+             * -0.5 to -1:      too late
+             * no click at all: miss
+             **/
+
+            // I was trying to use a switch case but apparently those only accept actual values, not just conditions.
+            if (clickTiming >= 0.5) Debug.Log("Early!");
+            else if (clickTiming >= 0.25) Debug.Log("Good.");
+            else if (clickTiming >= -0.25) Debug.Log("PERFECT!!!!");
+            else if (clickTiming >= -0.5) Debug.Log("Good.");
+            else if (clickTiming >= -1) Debug.Log("Late!");
+            else Debug.Log("How??!?!?! This is not supposed to happen.");
+        } else {
+            Debug.Log("Not playing, fool!");
+        }
     }
 
     void startLevel(int level) {
